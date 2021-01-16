0e646ca838531c5d38bf133699443df39fb9aa48
Harrison Blake <harrison.r.blake@gmail.com>
2021-01-15 23:45:20 -0700

    Finish all methods and tests to get final shifts

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,14 +1,75 @@
+require "time"
 class Enigma
   attr_reader :message, :key, :date
 
-  def initialize(message, key = nil, date = nil)
+  def initialize(message)
     @message = message
-    @key = key
-    @date = date
   end
 
-  def generate_random_key
+  def encrypt(message, key = generate_random_key, date = Time.now)
+
+  end
+
+  def get_final_shifts(date)
+    keys = get_keys
+    offsets = return_offsets(date)
+    shifts = []
+
+    4.times do |i|
+      shifts << offsets[i] + keys[i]
+    end
+
+    shifts
+  end
+
+  def get_keys
+    number_array = generate_random_number_array
+    keys = []
+
+    4.times do |i|
+      key = number_array[i..i+1].join
+      keys << key
+    end
+
+    keys.map do |key|
+      key.to_i
+    end
+  end
+
+  def generate_random_number_array
     random_number = rand(100000)
-    '%05d' % random_number
+    string_rand = '%05d' % random_number
+
+    string_rand.split("").map do |char|
+      char.to_i
+    end
+  end
+
+  def return_offsets(date)
+    f_date = return_date_formatted(date)
+    last_four = square_formatted_date_return_last_4(f_date)
+    offsets = last_four.split("")
+
+    offsets.map do |i|
+      i.to_i
+    end
+  end
+
+  def return_date_formatted(date)
+    year = "#{date.year}"
+    month = "#{date.month}"
+    day = "#{date.day}"
+
+    month.prepend("0") if month.length == 1
+
+    date_as_string = day + month + year[-2..-1]
+    date_as_string.to_i
+  end
+
+  def square_formatted_date_return_last_4(formatted_date)
+    squared = formatted_date * formatted_date
+    squared_as_string = squared.to_s
+
+    squared_as_string[-4..-1]
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/enigma'
+require 'mocha/minitest'
 
 class EnigmaTest < Minitest::Test
   def setup
@@ -8,14 +9,49 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_enigma_attributes
-
     assert_instance_of Enigma, @enigma
     assert_equal "Hello World", @enigma.message
-    assert_nil @enigma.key, @enigma.date
   end
 
-  def test_generate_random_key
+  def test_generate_random_number_array
+    assert_equal 5, @enigma.generate_random_number_array.length
+  end
 
-    assert_equal 5, @enigma.generate_random_key.length
+  def test_return_date_formatted_custom_date
+    time = Time.new(1995, 8, 4)
+
+    assert_equal 40895, @enigma.return_date_formatted(time)
+  end
+
+  def test_return_date_formatted_date_now
+    time = Time.now
+
+    assert_equal 150121, @enigma.return_date_formatted(time)
+  end
+
+  def test_square_formatted_date_return_last_4
+    time = Time.new(1995, 8, 4)
+    formatted_date = @enigma.return_date_formatted(time)
+
+    assert_equal "1025", @enigma.square_formatted_date_return_last_4(formatted_date)
+  end
+
+  def test_return_offsets
+    date = Time.new(1995, 8, 4)
+
+    assert_equal [1, 0, 2, 5], @enigma.return_offsets(date)
+  end
+
+  def test_get_keys
+    @enigma.stubs(:generate_random_number_array).returns([0,2,7,1,5])
+
+    assert_equal [02, 27, 71, 15], @enigma.get_keys
+  end
+
+  def test_get_final_shifts
+    date = Time.new(1995, 8, 4)
+    @enigma.stubs(:generate_random_number_array).returns([0,2,7,1,5])
+
+    assert_equal [3, 27, 73, 20], @enigma.get_final_shifts(date)
   end
 end
